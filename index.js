02c6ca5cec5f266b147130043cd2cc29c5e8beb2
Denis Baryshev <dennybaa@gmail.com>
2021-08-08 15:10:17 +0300

    upgrade semver parse action
    Signed-off-by: Denis Baryshev <dennybaa@gmail.com>

--- index.js
+++ index.js
@@ -1,34 +1,41 @@
+const core = require('@actions/core');
 const extractor = require('./src/extractor.js');
 const parser = require('./src/parser.js');
 const githubActionsIO = require('./src/actions_io.js');
 
-const INPUT_INPUT_STRING = 'input_string';
-const INPUT_VERSION_EXTRACTOR_REGEX = 'version_extractor_regex';
-
-const OUTPUT_MAJOR = 'major';
-const OUTPUT_MINOR = 'minor';
-const OUTPUT_PATCH = 'patch';
-const OUTPUT_PRE_RELEASE = 'prerelease';
-const OUTPUT_BUILD = 'build';
-const OUTPUT_FULL_VERSION = 'fullversion';
-
 function runAction(getInput, writeOutput) {
-    const inputString = getInput(INPUT_INPUT_STRING, true);
-    const extractorRegex = getInput(INPUT_VERSION_EXTRACTOR_REGEX);
+    const inputString = getInput('string', true);
+    const extractorRegex = getInput('extractor_regex');
+    const extractorGroup = getInput('extractor_group') || '1';
+    const failOnError = getInput('fail-on-error');
 
     versionString = inputString;
-    if (extractorRegex) {
-        versionString = extractor.extractWithRegexString(inputString, extractorRegex);
+
+    try {
+        if (extractorRegex) {
+            versionString = extractor.extractWithRegexString(inputString, extractorRegex, extractorGroup);
+        }
+
+        semanticVersion = parser.parse(versionString);
+
+        writeOutput('major', semanticVersion.major);
+        writeOutput('minor', semanticVersion.minor);
+        writeOutput('patch', semanticVersion.patch);
+        writeOutput('prerelease', semanticVersion.preRelease);
+        writeOutput('build', semanticVersion.build);
+        writeOutput('fullversion', semanticVersion.fullVersion);
+        writeOutput('version', `${semanticVersion.major}.${semanticVersion.minor}.${semanticVersion.patch}`);
+        writeOutput('minorversion', `${semanticVersion.major}.${semanticVersion.minor}`);
+        
     }
-
-    semanticVersion = parser.parse(versionString);
-
-    writeOutput(OUTPUT_MAJOR, semanticVersion.major);
-    writeOutput(OUTPUT_MINOR, semanticVersion.minor);
-    writeOutput(OUTPUT_PATCH, semanticVersion.patch);
-    writeOutput(OUTPUT_PRE_RELEASE, semanticVersion.preRelease);
-    writeOutput(OUTPUT_BUILD, semanticVersion.build);
-    writeOutput(OUTPUT_FULL_VERSION, semanticVersion.fullVersion);
+    catch (error) {
+        writeOutput('failed', 'true');
+        if (failOnError) {
+            core.setFailed(error.message);
+        } else {
+            core.warning(error.message);
+        }
+    }
 }
 
 runAction(githubActionsIO.getInput, githubActionsIO.writeOutput);
--- src/extractor.js
+++ src/extractor.js
@@ -1,17 +1,15 @@
 
-function extractWithRegexString(inputString, regexString)
+function extractWithRegexString(inputString, regexString, regexGroup='1')
 {
+    const group = regexGroup.toString()
     const regex = new RegExp(regexString, 'g');
     matches = regex.exec(inputString);
 
     if (null === matches) {
-        throw new Error("Error: No regex match found");
-    }
-    if (2 < matches.length) {
-        throw new Error("Error: Multiple regex matches found");
+        throw new Error("Could not extract version using the given regex!");
     }
 
-    return matches[1];
+    return matches[group];
 }
 
 exports.extractWithRegexString = extractWithRegexString;
